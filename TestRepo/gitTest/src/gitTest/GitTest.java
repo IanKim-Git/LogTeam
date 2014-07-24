3e2e656a94e3bc24dfe3488efc675c6bfaf0102d
IanKim-Git <everybob@gmail.com>
2014-07-24 17:38:24 +0900

    KIM@@@!!!

--- TestRepo/gitTest/src/gitTest/GitTest.java
+++ TestRepo/gitTest/src/gitTest/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello ProgManager!!!");
 		System.out.println("I'm state park");
+		
+		System.out.println("It's kIm!!!!!!!");
 	}
 }
