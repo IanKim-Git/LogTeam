7e680ae4a56c29ac5d76eeec281a44dd4ad72663
상태 <@ParkCOM>
2014-07-24 17:31:33 +0900

    hi2

--- TestRepo/gitTest/src/gitTest/GitTest.java
+++ TestRepo/gitTest/src/gitTest/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello ProgManager!!!");
-		System.out.println("I'm stat park");
+		System.out.println("I'm state park");
 	}
 }
