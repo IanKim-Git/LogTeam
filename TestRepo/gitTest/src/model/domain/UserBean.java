eabd5cb2b821c7819637f742271001d059a67dff
DaEunPark <daEun@daEun-PC>
2014-08-05 16:45:19 +0900

    beanTest

---
+++ TestRepo/gitTest/src/model/domain/UserBean.java
@@ -0,0 +1,14 @@
+package model.domain;
+
+public class UserBean {
+	String umail;
+	String upw;
+//	uphoto;
+	String uname;
+	String uphone;
+	int uprocnt;
+	public UserBean() {
+		super();
+	}
+	
+}
