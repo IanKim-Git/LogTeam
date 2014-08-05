386f4dc5eab4c3b159e9cb169d1938eac6ada709
IanKim-Git <everybob@gmail.com>
2014-08-05 16:53:09 +0900

    qwe

--- TestRepo/gitTest/src/gitTest/GitTest.java
+++ TestRepo/gitTest/src/gitTest/GitTest.java
@@ -10,5 +10,12 @@ public class GitTest {
 		System.out.println("I'm Da Eun Park.");
 		
 		System.out.println("test");
+		
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		
+		
 	}
 }
