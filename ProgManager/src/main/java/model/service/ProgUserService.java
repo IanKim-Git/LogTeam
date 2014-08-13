f7d5413ee60289da3d5349ae2bc2d209557c296d
Hwang <hsunam12@naver.com>
2014-08-13 13:48:33 +0900

    프로젝트 정보 보기까지 UI완성

--- ProgManager/src/main/java/model/service/ProgUserService.java
+++ ProgManager/src/main/java/model/service/ProgUserService.java
@@ -6,31 +6,36 @@ import model.dao.ProgUserDAO;
 import model.domain.ProgUserBean;
 
 public class ProgUserService {
-	@Resource(name="puDao")
+	@Resource(name = "puDao")
 	private ProgUserDAO puDao;
-	
-	//로그인 할 때 아이디(이메일)와 이메일 체크 후 결과 반환
-	public ProgUserBean userCheck(String uemail, String upw){
+
+	// 로그인 할 때 아이디(이메일)와 이메일 체크 후 결과 반환
+	public ProgUserBean userCheck(String uemail, String upw) {
 		return puDao.userIdPwCheck(uemail, upw);
 	}
-	
-	//회원가입 후 결과 반환
-	public int userSignIn(ProgUserBean pu){
+
+	// 회원가입 후 결과 반환
+	public int userSignIn(ProgUserBean pu) {
 		return puDao.userSignIn(pu);
 	}
-	
-	//이메일 중복 체크 결과 반환
-	public String userEmailCheck(String uemail){
+
+	// 이메일 중복 체크 결과 반환
+	public String userEmailCheck(String uemail) {
 		return puDao.userIdCheck(uemail);
 	}
-	
-	//개인 정보 변경
-	public int userUpdateInfo(ProgUserBean pu){
+
+	// 개인 정보 변경
+	public int userUpdateInfo(ProgUserBean pu) {
 		return puDao.userUpdate(pu);
 	}
-	
-	//개인 정보 반환
-	public ProgUserBean userInfo(String uemail){
+
+	// 회원 탈퇴
+	public int userDelete(ProgUserBean pu) {
+		return puDao.userDelete(pu);
+	}
+
+	// 개인 정보 반환
+	public ProgUserBean userInfo(String uemail) {
 		return puDao.userInfo(uemail);
 	}
 }
